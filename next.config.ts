d82f2c9b48dcf25105af71d0f296db5925cd8428
Shambhavi-Bhalekar <bhalekarshambhavi.15@gmail.com>
2025-10-31 11:44:51 +0530

    fixed the turbopack issue

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
-module.exports = {
-  experimental: {
-    turbopack: false, // Disable Turbopack
-  },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+module.exports = nextConfig;
